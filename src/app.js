59aba0117d9d4c0d8a1eb5d8520997f60f003b1a
av1-lv5 <90096511+Av1-Lv5@users.noreply.github.com>
2023-04-04 05:03:37 +0530

    Converted 1,0 to true, false. Improved data type.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 let turn;
 let movesCounter = 0;
 const scoreCounter = {
-    x: 0,
-    tie: 0,
-    o: 0,
+	x: 0,
+	tie: 0,
+	o: 0,
 };
 
 // the winStates are used as classnames for respective html elements.
@@ -11,16 +11,16 @@ const winStates = ["r1", "r2", "r3", "c1", "c2", "c3", "d1", "d2"];
 
 // class to create X and O win states counters.
 class CreateWinStatesCounters {
-    constructor() {
-        winStates.forEach((winState) => {
-            this[winState] = 0;
-        });
-    }
+	constructor() {
+		winStates.forEach((winState) => {
+			this[winState] = 0;
+		});
+	}
 }
 
 const winStatesCounters = {
-    x: new CreateWinStatesCounters(),
-    o: new CreateWinStatesCounters(),
+	x: new CreateWinStatesCounters(),
+	o: new CreateWinStatesCounters(),
 };
 
 // ----------  SELECTORS ----------
@@ -68,23 +68,23 @@ newGameVsPlayer.addEventListener("click", startNewGame);
 
 // game page
 restartBtn.addEventListener("click", () => {
-    toggleModal("restart", true);
+	toggleModal("restart", true);
 });
 cells.forEach((cell) => {
-    cell.addEventListener("click", cellOnClick);
-    cell.addEventListener("mouseover", (e) => {
-        cellHover(e.target, true);
-    });
-    cell.addEventListener("mouseout", (e) => {
-        cellHover(e.target, false);
-    });
-    // cell.setAttribute("onmouseout", "cellHover(this)");
+	cell.addEventListener("click", cellOnClick);
+	cell.addEventListener("mouseover", (e) => {
+		cellHover(e.target, true);
+	});
+	cell.addEventListener("mouseout", (e) => {
+		cellHover(e.target, false);
+	});
+	// cell.setAttribute("onmouseout", "cellHover(this)");
 });
 
 // restart, results modals
 confirmRestartBtn.addEventListener("click", confirmRestart);
 cancelRestartBtn.addEventListener("click", () => {
-    toggleModal("restart", false);
+	toggleModal("restart", false);
 });
 quitGameBtn.addEventListener("click", quitGame);
 nextRoundBtn.addEventListener("click", nextRound);
@@ -94,164 +94,164 @@ nextRoundBtn.addEventListener("click", nextRound);
 // Functions to Pick X and O as 1p mark.
 
 function selectX() {
-    pickO.classList.remove("selected");
-    pickOImg.setAttribute("src", "assets/icon-o-silver.svg");
-    pickX.classList.add("selected");
-    pickXImg.setAttribute("src", "assets/icon-x-dark.svg");
+	pickO.classList.remove("selected");
+	pickOImg.setAttribute("src", "assets/icon-o-silver.svg");
+	pickX.classList.add("selected");
+	pickXImg.setAttribute("src", "assets/icon-x-dark.svg");
 }
 
 function selectO() {
-    pickX.classList.remove("selected");
-    pickXImg.setAttribute("src", "assets/icon-x-silver.svg");
-    pickO.classList.add("selected");
-    pickOImg.setAttribute("src", "assets/icon-o-dark.svg");
+	pickX.classList.remove("selected");
+	pickXImg.setAttribute("src", "assets/icon-x-silver.svg");
+	pickO.classList.add("selected");
+	pickOImg.setAttribute("src", "assets/icon-o-dark.svg");
 }
 
 // start a new game with selected mark and opponent.
 function startNewGame() {
-    startPage.classList.add("hide");
-    gamePage.classList.remove("hide");
+	startPage.classList.add("hide");
+	gamePage.classList.remove("hide");
 
-    turn = "x";
-    updateTurnIndicator();
-    clearGameBoard();
-    resetScoreCounters();
+	turn = "x";
+	updateTurnIndicator();
+	clearGameBoard();
+	resetScoreCounters();
 }
 
 function cellHover(cell, hoverStatus) {
-    let srcValue;
+	let srcValue;
 
-    hoverStatus
-        ? (srcValue = `assets/icon-${turn}-outline.svg`)
-        : (srcValue = "");
+	hoverStatus
+		? (srcValue = `assets/icon-${turn}-outline.svg`)
+		: (srcValue = "");
 
-    if (!cell.classList.contains("clicked-cell")) {
-        turn === "x"
-            ? cell.firstElementChild.setAttribute("src", srcValue)
-            : cell.firstElementChild.setAttribute("src", srcValue);
-    }
+	if (!cell.classList.contains("clicked-cell")) {
+		turn === "x"
+			? cell.firstElementChild.setAttribute("src", srcValue)
+			: cell.firstElementChild.setAttribute("src", srcValue);
+	}
 }
 
 // a series of actions to take place in sequential order, when a cell on game-board is clicked.
 function cellOnClick(e) {
-    const clickedCell = e.target;
-    let endGame = 0;
+	const clickedCell = e.target;
+	let endGame = false;
 
-    if (!clickedCell.classList.contains("clicked-cell")) {
-        clickedCell.classList.add("clicked-cell");
-        movesCounter++; // update moves counter
+	if (!clickedCell.classList.contains("clicked-cell")) {
+		clickedCell.classList.add("clicked-cell");
+		movesCounter++; // update moves counter
 
-        // Marks the cells with X,O.
-        clickedCell.firstElementChild.setAttribute(
-            "src",
-            `assets/icon-${turn}.svg`
-        );
+		// Marks the cells with X,O.
+		clickedCell.firstElementChild.setAttribute(
+			"src",
+			`assets/icon-${turn}.svg`
+		);
 
-        clickedCell.classList.forEach((classItem) => {
-            if (classItem !== "cell" && classItem !== "clicked-cell") {
-                winStatesCounters[turn][classItem] += 1; // update winStates counters
+		clickedCell.classList.forEach((classItem) => {
+			if (classItem !== "cell" && classItem !== "clicked-cell") {
+				winStatesCounters[turn][classItem] += 1; // update winStates counters
 
-                // check for win
-                if (
-                    movesCounter > 4 &&
-                    winStatesCounters[turn][classItem] === 3
-                ) {
-                    scoreCounter[turn]++;
-                    endGame = 1;
-                    resultHead.innerText = `${turn} WON`;
-                }
-            }
-        });
+				// check for win
+				if (
+					movesCounter > 4 &&
+					winStatesCounters[turn][classItem] === 3
+				) {
+					scoreCounter[turn]++;
+					endGame = true;
+					resultHead.innerText = `${turn} WON`;
+				}
+			}
+		});
 
-        if (movesCounter === 9 && !endGame) {
-            scoreCounter.tie++;
-            endGame = 1;
-            resultHead.innerText = "Tie match";
-        }
-        if (endGame) {
-            updateScoreBoard();
-            toggleModal("results", true);
-        }
+		if (movesCounter === 9 && !endGame) {
+			scoreCounter.tie++;
+			endGame = true;
+			resultHead.innerText = "Tie match";
+		}
+		if (endGame) {
+			updateScoreBoard();
+			toggleModal("results", true);
+		}
 
-        turn === "x" ? (turn = "o") : (turn = "x"); // switches turn value
-        updateTurnIndicator();
-    }
+		turn === "x" ? (turn = "o") : (turn = "x"); // switches turn value
+		updateTurnIndicator();
+	}
 }
 
 // Updates the turn indicator image
 function updateTurnIndicator() {
-    turn === "x"
-        ? turnIndicatorImg.setAttribute("src", "assets/icon-x.svg")
-        : turnIndicatorImg.setAttribute("src", "assets/icon-o.svg");
+	turn === "x"
+		? turnIndicatorImg.setAttribute("src", "assets/icon-x.svg")
+		: turnIndicatorImg.setAttribute("src", "assets/icon-o.svg");
 }
 
 // updates scoreboard
 function updateScoreBoard() {
-    xScore.innerText = scoreCounter.x;
-    oScore.innerText = scoreCounter.o;
-    tieScore.innerText = scoreCounter.tie;
+	xScore.innerText = scoreCounter.x;
+	oScore.innerText = scoreCounter.o;
+	tieScore.innerText = scoreCounter.tie;
 }
 
 // Clean board for a new round/game
 function clearGameBoard() {
-    // removes visual changes of a clicked cell
-    cells.forEach((cell) => {
-        cell.classList.remove("clicked-cell");
-        cell.firstElementChild.setAttribute("src", "");
-    });
+	// removes visual changes of a clicked cell
+	cells.forEach((cell) => {
+		cell.classList.remove("clicked-cell");
+		cell.firstElementChild.setAttribute("src", "");
+	});
 
-    movesCounter = 0; // reset moves counter
+	movesCounter = 0; // reset moves counter
 
-    // resets winning states counters
-    winStates.forEach((winState) => {
-        winStatesCounters.x[winState] = 0;
-        winStatesCounters.o[winState] = 0;
-    });
+	// resets winning states counters
+	winStates.forEach((winState) => {
+		winStatesCounters.x[winState] = 0;
+		winStatesCounters.o[winState] = 0;
+	});
 }
 
 // ---------------- Counters ------------------------
 
 // resets score counters
 function resetScoreCounters() {
-    scoreCounter.x = 0;
-    scoreCounter.o = 0;
-    scoreCounter.tie = 0;
-    updateScoreBoard();
+	scoreCounter.x = 0;
+	scoreCounter.o = 0;
+	scoreCounter.tie = 0;
+	updateScoreBoard();
 }
 
 // ------------ Modals related functions ---------------
 
 // toggle modals visibility
 function toggleModal(modalName, visibile) {
-    console.log(modalName);
-    if (visibile) {
-        modalContainer.classList.remove("hide");
-        if (modalName === "results") resultModal.classList.remove("hide");
-        if (modalName === "restart") restartModal.classList.remove("hide");
-    } else {
-        modalContainer.classList.add("hide");
-        if (modalName === "results") resultModal.classList.add("hide");
-        if (modalName === "restart") restartModal.classList.add("hide");
-    }
+	console.log(modalName);
+	if (visibile) {
+		modalContainer.classList.remove("hide");
+		if (modalName === "results") resultModal.classList.remove("hide");
+		if (modalName === "restart") restartModal.classList.remove("hide");
+	} else {
+		modalContainer.classList.add("hide");
+		if (modalName === "results") resultModal.classList.add("hide");
+		if (modalName === "restart") restartModal.classList.add("hide");
+	}
 }
 
 // Quit entire game and return to start page
 function quitGame() {
-    toggleModal("results", false);
-    startPage.classList.remove("hide");
-    gamePage.classList.add("hide");
+	toggleModal("results", false);
+	startPage.classList.remove("hide");
+	gamePage.classList.add("hide");
 }
 
 // Restart game with previously choosen opponent and mark.
 function confirmRestart() {
-    clearGameBoard();
-    resetScoreCounters();
-    modalContainer.classList.add("hide");
-    restartModal.classList.add("hide");
+	clearGameBoard();
+	resetScoreCounters();
+	modalContainer.classList.add("hide");
+	restartModal.classList.add("hide");
 }
 
 // just another round, scores still counting.
 function nextRound() {
-    toggleModal("results", false);
-    clearGameBoard();
+	toggleModal("results", false);
+	clearGameBoard();
 }
